bad0d38e51a503324dd6224cbb491812ba510002
tony <201267151@qq.com>
2022-11-24 19:02:45 +0800

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/reset.css'
 import Card from './components/card/Card.vue'
 
 const app = createApp(App)
+const pinia = createPinia()
+
 app.component('Card',Card)
 // 全局组件注册
 app.use(router)
+app.use(pinia)
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import autoImport from 'unplugin-auto-import/vite'
+import autoImport from "unplugin-auto-import/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
 		autoImport({
-			imports:['vue','vue-router'],
-			dts:"src/auto-import.d.ts"
-	
-		})
+			imports: ["vue", "vue-router", "pinia"],
+			dts: "src/auto-import.d.ts",
+		}),
 	],
 });
